166a4d7e703e20b513d9680780a59f8e8723729d
dhirajs16 <sarrafd16@gmail.com>
2024-12-22 18:30:16 +0545

    secret key issue resolve with vite and import.meta

--- src/utils/axios.tsx
+++ src/utils/axios.tsx
@@ -1,21 +1,13 @@
 import axios from "axios"
 
+const authorization_key: string = import.meta.env.AUTHORIZATION_KEY  
+
 const instance = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   headers: {
-<<<<<<< HEAD
     accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNDZjNTYwZDUxNjkzZDU2N2YwNTk4ZmJmOTNjYmQzYSIsIm5iZiI6MTczNDQxODQ3MC41NDgsInN1YiI6IjY3NjEyMDI2YjgwYTk3NDI0NWNiZGI4NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xP0gciqXhK8RN_yslj6PFMvoiJFXnYB7tygO-txuSpI'
+    Authorization: authorization_key
   }
 })
 
 export default instance
-=======
-    accept: "application/json",
-    Authorization:
-  },
-});
-
-export default instance;
-// https://developer.themoviedb.org/reference/account-lists
->>>>>>> 18a1979448b73936511d442eb6002ddea36a72a0
